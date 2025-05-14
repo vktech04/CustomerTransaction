1e0854b2015a948eb6b74f5f6c5ef58f79bcfe78
vktech04 <kulkarnivaishu9504@gmail.com>
2025-05-14 15:41:35 +0530

    saved

--- CustomerTransaction.java
+++ CustomerTransaction.java
@@ -17,10 +17,44 @@ class BankCustomer {
     }
 }
 
+class TransactionHistory {
+    String transactionId;
+    String senderName;
+    String receiverName;
+    double amount;
+    String status;
+    String date;
+
+    TransactionHistory(String transactionId, String senderName, String receiverName, double amount, String status, String date) {
+        this.transactionId = transactionId;
+        this.senderName = senderName;
+        this.receiverName = receiverName;
+        this.amount = amount;
+        this.status = status;
+        this.date = date;
+    }
+
+    void display() {
+        System.out.println("Transaction ID: " + transactionId);
+        System.out.println("Sender Name: " + senderName);
+        System.out.println("Receiver Name: " + receiverName);
+        System.out.println("Amount: $" + amount);
+        System.out.println("Status: " + status);
+        System.out.println("Date: " + date);
+        System.out.println("---------------------------");
+    }
+}
+
+
 public class CustomerTransaction {
     public static void main(String[] args) {
         BankCustomer customer = new BankCustomer("Vaishnavi", "Sa237", 21);
         customer.display();
 
+        TransactionHistory t1 = new TransactionHistory("T1001", "Vaishnavi", "Usha", 5000.0, "Successful", "10-05-2025");
+        t1.display();
+
+        TransactionHistory t2 = new TransactionHistory("T2002", "Usha", "Vaishnavi", 2000.0, "Failed", "11-05-2025");
+        t2.display();
     }
 }
